468b8c17bb97f179b7b45473dcb1a191f60bb552
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2025-01-06 06:43:34 -0600

    Update changes

--- src/handlers.rs
+++ src/handlers.rs
@@ -32,15 +32,15 @@ pub async fn create_user(pool: web::Data<sqlx::Pool<sqlx::Mssql>>,new_user: web:
         r#"
         INSERT INTO [users] (
             id,
-            User_id,
+            UserId,
             Name,
-            Last_name,
+            LastName,
             Email,
             Age,
             Phone,
             Address,
             BirthDate,
-            Place_birth
+            PlaceBirth
         )
         VALUES (
             @p1, @p2, @p3, @p4, @p5,
@@ -84,14 +84,14 @@ pub async fn create_user(pool: web::Data<sqlx::Pool<sqlx::Mssql>>,new_user: web:
 ///
 /// ```
 /// use actix_web::{web, App, HttpServer};
-/// use safe_user::handlers::get_jwt;
+/// use safe_user::handlers::create_jwt_for_user;
 /// use safe_user::models::User;
 ///
 /// #[actix_web::main]
 /// async fn main() -> std::io::Result<()> {
 ///     HttpServer::new(|| {
 ///         App::new()
-///             .route("/get_jwt", web::post().to(get_jwt))
+///             .route("/get_jwt", web::post().to(create_jwt_for_user))
 ///     })
 ///     .bind("127.0.0.1:8080")?
 ///     .run()
@@ -100,7 +100,7 @@ pub async fn create_user(pool: web::Data<sqlx::Pool<sqlx::Mssql>>,new_user: web:
 ///```
 pub async fn create_jwt_for_user(info: web::Json<User>) -> impl Responder {
     let response = match generate_jwt(Some(&info.id.clone().expect("REASON").to_string()).unwrap()) {
-        Ok(token) => format!("JWT: {}", token),
+        Ok(token) => format!("{}", token),
         Err(_) => "Failed to generate JWT".to_string(),
     };
 
@@ -125,7 +125,7 @@ pub async fn create_jwt_for_user(info: web::Json<User>) -> impl Responder {
 ///
 /// ```
 /// use actix_web::{web, App, HttpServer};
-/// use safe_user::handlers::get_users;
+/// use safe_user::handlers::get_all_users;
 /// use safe_user::db::DbPool;
 ///
 /// #[actix_web::main]
@@ -134,7 +134,7 @@ pub async fn create_jwt_for_user(info: web::Json<User>) -> impl Responder {
 ///     HttpServer::new(move || {
 ///         App::new()
 ///             .app_data(web::Data::new(pool.clone()))
-///             .route("/get_users", web::get().to(get_users))
+///             .route("/get_users", web::get().to(get_all_users))
 ///     })
 ///     .bind("127.0.0.1:8080")?
 ///     .run()
@@ -147,20 +147,20 @@ pub async fn get_all_users(pool: web::Data<Pool<Mssql>>) -> impl Responder {
         r#"
         SELECT
             CAST(id AS VARCHAR(36))         AS "id?", -- Cast UUID to String
-            User_id                         AS "user_id!",
+            UserId                          AS "user_id!",
             Name                            AS "name!",
-            Last_name                       AS "last_name!",
+            LastName                        AS "last_name!",
             Email                           AS "email!",
             Age                             AS "age?",
             Phone                           AS "phone?",
             Address                         AS "address?",
             CONVERT(VARCHAR, BirthDate, 23) AS "birthdate!",
-            Place_birth                     AS "place_birth?"
+            PlaceBirth                      AS "place_birth?"
         FROM [users]
         "#
     )
-        .fetch_all(pool.get_ref())
-        .await;
+    .fetch_all(pool.get_ref())
+    .await;
 
     match query_result {
         Ok(users) => HttpResponse::Ok().json(users),
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
-mod db;
-mod handler;
-mod auth;
-mod models;
+use actix_web::{web, App, HttpServer};
+use actix_web_httpauth::middleware::HttpAuthentication;
+use safe_user::db::DbPool;
+use safe_user::handlers::{create_user, create_jwt_for_user, get_all_users, protected_route};
+use safe_user::auth::jwt_validator;
+use dotenv::dotenv;
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let db_pool = DbPool::new().await.expect("No se pudo crear la conexión a la base de datos.");
+    let pool_data = web::Data::new(db_pool.pool);
+
+    HttpServer::new(move || {
+        let auth = HttpAuthentication::bearer(jwt_validator);
+
+        App::new()
+            .app_data(pool_data.clone())
+            .route("/create_user", web::post().to(create_user))
+            .route("/get_jwt", web::post().to(create_jwt_for_user))
+            .service(
+                web::scope("/protected")
+                    .wrap(auth)
+                    .route("/users", web::get().to(get_all_users))
+                    .route("/route", web::get().to(protected_route))
+            )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
